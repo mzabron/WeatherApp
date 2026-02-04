fd3a8c6722088b6e90ab584a368307bafb2c8b97
Maksym Ząbroń <mzabron@student.agh.edu.pl>
2026-02-04 14:00:43 +0100

    add function to fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,46 @@
 import "./App.css";
+import {useState, useEffect} from 'react';
+import { mockWeather } from "./api/mockWeather";
+import type {WeatherData} from "./types/weather";
 
 export default function App()
 {
   return(
-    <div>
+    <div className = "App">
       <h1>Weather App</h1>
+      <hr />
+      <FetchWeatherData />
     </div>
+  );
+}
+
+function FetchWeatherData()
+{
+  const [weather, setWeather] = useState<WeatherData | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        const data = await mockWeather();
+        setWeather(data);
+      } catch(error) {
+        console.log("loading data failed",error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadData();
+  },[]);
+
+  if(loading){
+  return(
+    <p>Loading...</p>
   )
+  } else if (weather){
+    return(
+      <p>{weather.location.name}</p>
+    )
+  }
+  return <p>No data available</p>
 }
